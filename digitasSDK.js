1e709418d5a0acb686dd01b0e402e4bb44117e19
nicddominguez <69926866+nicddominguez@users.noreply.github.com>
2023-10-27 11:30:04 -0300

    Update digitasSDK.js

--- digitasSDK.js
+++ digitasSDK.js
@@ -1,7 +1,14 @@
 class DigitasSDK {
-  consoleLog(a) {
-    console.log(a);
-    return a;
+  post(url, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(data));
+  }
+
+  test() {
+    console.log("test");
+    return "test";
   }
 }
 
