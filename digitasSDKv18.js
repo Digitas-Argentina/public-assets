86463724d9ef30afd44a780aca246ef37cbe0fef
nicddominguez <69926866+nicddominguez@users.noreply.github.com>
2023-11-01 16:30:38 -0300

    Add files via upload

---
+++ digitasSDKv18.js
@@ -0,0 +1,33 @@
+class DigitasSDK {
+  post(url, data) {
+    console.log("digitasSDK.post");
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        var jsonRes = JSON.parse(xhr.responseText);
+        console.log("XMLHttpRequest done");
+        console.log(jsonRes);
+      }
+    };
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(data));
+  }
+
+  getClientIp() {
+    console.log("digitasSDK.getClientIp");
+    return fetch("https://api.ipify.org").then(res => res.text());
+  }
+
+  getUserAgent() {
+    console.log("digitasSDK.getUserAgent");
+    return navigator.userAgent;
+  }
+
+  test() {
+    console.log("digitasSDK.test");
+    return "test";
+  }
+}
+
+window.digitasSDK = new DigitasSDK();
